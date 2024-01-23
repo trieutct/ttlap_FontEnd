0543b2ee6b1f794c0b70f3c312d16142bd0bfbd2
trieutct <96654274+trieutct@users.noreply.github.com>
2024-01-23 15:10:29 +0700

    xong phan vee-validation and yup trang login

--- src/views/Login/LoginView.vue
+++ src/views/Login/LoginView.vue
@@ -1,26 +1,31 @@
 <template>
     <div>
         <v-card class="mx-auto pa-12 pb-8 my-6" variant="flat" max-width="500" rounded="lg">
-            <v-img class="mx-auto my-6" max-width="100" src="https://tse4.mm.bing.net/th?id=OIP.fv5DeUz-OmV84NOdioiodgHaEc&pid=Api&P=0&h=220"></v-img>
+            <v-img class="mx-auto my-6" max-width="100"
+                src="https://tse4.mm.bing.net/th?id=OIP.fv5DeUz-OmV84NOdioiodgHaEc&pid=Api&P=0&h=220"></v-img>
             <div class="text-h4 text-center">
                 Đăng nhập
             </div>
-            <div class="text-subtitle-1 text-medium-emphasis">Email</div>
-            <v-text-field density="compact" placeholder="Nhập email" variant="outlined"></v-text-field>
-            <div class="text-subtitle-1 text-medium-emphasis d-flex align-center justify-space-between">
-                Mật khẩu
-            </div>
-            <v-text-field :append-inner-icon="visible ? 'mdi-eye' : 'mdi-eye-off'" :type="visible ? 'text' : 'password'"
-                density="compact" placeholder="Nhập mật khẩu" variant="outlined"
-                @click:append-inner="visible = !visible"></v-text-field>
-            <div class="d-flex justify-space-between align-center">
-                <v-checkbox v-model="terms" color="secondary" label="Ghi nhớ Đăng nhập"></v-checkbox>
-                <a class="text-caption text-decoration-none text-blue mb-6">
-                    Quên mật khẩu?</a>
-            </div>
-            <v-btn block class="mb-8" color="#0F60FF" size="large">
-                Đăng nhập
-            </v-btn>
+            <v-form @submit.prevent="login">
+                <div class="text-subtitle-1 text-medium-emphasis">Email</div>
+                <v-text-field v-model="email" type="email" :error-messages="emailError" required density="compact"
+                    placeholder="Nhập email" variant="outlined"></v-text-field>
+                <div class="text-subtitle-1 text-medium-emphasis d-flex align-center justify-space-between">
+                    Mật khẩu
+                </div>
+                <v-text-field v-model="password" :error-messages="passwordError" required
+                    :append-inner-icon="visible ? 'mdi-eye' : 'mdi-eye-off'" :type="visible ? 'text' : 'password'"
+                    density="compact" placeholder="Nhập mật khẩu" variant="outlined"
+                    @click:append-inner="visible = !visible"></v-text-field>
+                <div class="d-flex justify-space-between align-center">
+                    <v-checkbox v-model="terms" color="secondary" label="Ghi nhớ Đăng nhập"></v-checkbox>
+                    <a class="text-caption text-decoration-none text-blue mb-6">
+                        Quên mật khẩu?</a>
+                </div>
+                <v-btn type="submit" block class="mb-8" color="#0F60FF" size="large">
+                    Đăng nhập
+                </v-btn>
+            </v-form>
             <div class="text-center">
                 Bạn chưa có tài khoản ?
                 <a class="text-decoration-none text-center" style="color:#0F60FF;">
@@ -30,8 +35,7 @@
         </v-card>
     </div>
 </template>
-
-<script>
+<!-- <script>
 export default {
     name: 'LoginView',
     data() {
@@ -41,6 +45,33 @@ export default {
         }
     }
 }
+</script> -->
+<script setup>
+import { useForm, useField } from 'vee-validate';
+import * as yup from 'yup';
+import { ref } from 'vue';
+
+const visible = ref(false)
+const terms = ref('')
+
+const { handleSubmit } = useForm();
+
+const { value: email, errorMessage: emailError } = useField(
+    'email',
+    yup.string().required("Không được bỏ trống").email("Email không phù hợp")
+);
+
+const { value: password, errorMessage: passwordError } = useField(
+    'password',
+    yup.string().required("Không được bỏ trống email").min(6, "Chưa đủ 6 ký tự")
+);
+const login = handleSubmit(async () => {
+    alert(email.value+ "   "+password.value)
+    console.log('Logging in with:', email, password);
+});
+
+
 </script>
+
 
 <style></style>
